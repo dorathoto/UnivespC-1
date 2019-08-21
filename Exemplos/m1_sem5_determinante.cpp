32e01952e584c83eb4ad0142d6c28c68684c0565
Alejandro Druetta <aledruetta@gmail.com>
2019-08-21 08:06:58 -0300

    comentários

--- Exemplos/m1_sem5_determinante.cpp
+++ Exemplos/m1_sem5_determinante.cpp
@@ -22,9 +22,13 @@ using namespace std;
 
 int main ()
 {
+    // Declara uma matriz de 3x5
     int M[SIZE][SIZE+2];
 
     cout << "\n*** Cálculo Determinante *** \n" << endl;
+
+    // Ingresa os valores da matriz
+    // Três valores por linha
     for (unsigned i=0; i<SIZE; i++) {
         cout << "Linha " << i << ": ";
         cin >> M[i][0]
@@ -32,12 +36,14 @@ int main ()
             >> M[i][2];
     }
 
+    // Copia as duas primeiras colunas
     for (unsigned i=0; i<SIZE; i++) {
         for (unsigned j=0; j<SIZE-1; j++) {
             M[i][j+SIZE] = M[i][j];
         }
     }
 
+    // Aplica o algoritmo para o cálculo
     int deter = 0;
     for (unsigned i=0; i<SIZE; i++) {
         deter += M[0][i] * (M[1][i+1] * M[2][i+2] - M[1][i+2] * M[2][i+1]);
